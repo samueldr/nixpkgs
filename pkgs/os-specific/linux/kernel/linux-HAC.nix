64fddc2a77314c619da78dee09aa50d088d3e21d
Samuel Dionne-Riel <samuel@dionne-riel.com>
2018-04-27 23:15:33 +0000

    WIP: tries again using the manual build

--- pkgs/os-specific/linux/kernel/linux-HAC.nix
+++ pkgs/os-specific/linux/kernel/linux-HAC.nix
@@ -1,86 +1,106 @@
-#{ stdenv, hostPlatform
-#, fetchFromGitHub
-#, linuxManualConfig
-#, bison, flex
-#, binutils-unwrapped
-#, kernelPatches ? [] }:
-#
-## Inspired by https://github.com/thefloweringash/rock64-nix/blob/master/packages/linux_ayufan_4_4.nix
-#
-#
-#let
-#  buildLinux = (args: (linuxManualConfig args).overrideAttrs ({ makeFlags, ... }: {
-#    # Necessary?
-#    postPatch = ''
-#      patchShebangs .
-#    '';
-#    makeFlags = makeFlags ++ [
-#      # Why do I have to do this??
-#      "OBJCOPY=${binutils-unwrapped}/bin/${stdenv.cc.targetPrefix}objcopy"
-#      "AR=${binutils-unwrapped}/bin/${stdenv.cc.targetPrefix}ar"
-#    ];
-#  }));
-#
-#  configfile = stdenv.mkDerivation {
-#    name = "HAC-linux-kernel-config-4.16";
-#    inherit version;
-#    inherit src;
-#    nativeBuildInputs = [bison flex];
-#
-#    buildPhase = ''
-#      make nintendo-switch_defconfig
-#    '';
-#
-#    installPhase = ''
-#      cp .config $out
-#    '';
-#  };
-#
-#in
-#
-#buildLinux {
-#  inherit stdenv kernelPatches;
-#  inherit hostPlatform;
-#  inherit src;
-#  inherit version;
-#
-#  modDirVersion = "4.16.0-rc1";
-#
-#  inherit configfile;
-#
-#  allowImportFromDerivation = true;
-#}
+{ stdenv, hostPlatform
+, fetchFromGitHub
+, linuxManualConfig
+, bison, flex
+, binutils-unwrapped
+, kernelPatches ? [] }:
 
-{ stdenv, buildPackages, hostPlatform, fetchFromGitHub, perl, buildLinux, ... } @ args:
+# Inspired by https://github.com/thefloweringash/rock64-nix/blob/master/packages/linux_ayufan_4_4.nix
+
 let
+  version = "4.16.0-rc1";
   src = fetchFromGitHub {
     owner = "fail0verflow";
     repo = "switch-linux";
     rev = "01b404c2112028a48ccf533b84218689032054c3";
     sha256 = "1x7bvc69rhnkx3cy4djsjk0hrc6ypdsnm3465v1gxmddd3fx1mjb";
   };
-  version = "4.16.0-rc1";
+in
+let
+  buildLinux = (args: (linuxManualConfig args).overrideAttrs ({ makeFlags, kernelPatches ? [], ... }: {
+    # Necessary?
+    postPatch = ''
+      patchShebangs .
+    '';
+    #makeFlags = makeFlags ++ [
+    #  # Why do I have to do this??
+    #  "OBJCOPY=${binutils-unwrapped}/bin/${stdenv.cc.targetPrefix}objcopy"
+    #  "AR=${binutils-unwrapped}/bin/${stdenv.cc.targetPrefix}ar"
+    #];
+    patches = [
+      ./HAC-uncross.diff
+    ];
+
+  }));
+
+  configfile = stdenv.mkDerivation {
+    name = "HAC-linux-kernel-config-4.16";
+    inherit version;
+    inherit src;
+    nativeBuildInputs = [bison flex];
+
+    buildPhase = ''
+      make nintendo-switch_defconfig
+    '';
+
+    installPhase = ''
+      cp .config $out
+    '';
+  };
+
 in
 
- buildLinux (args // {
-   inherit version;
-   inherit src;
- 
-   kernelPatches = args.kernelPatches;
- 
-   features.iwlwifi = true;
-   features.efiBootStub = false;
-   features.needsCifsUtils = true;
-   features.netfilterRPFilter = true;
- 
- #  # https://github.com/NixOS/nixpkgs/issues/35166#issuecomment-366594016
- #  # override aarch64-multiplatform settings.
- #  # not the right way to do this; need advice.
- #  hostPlatform = hostPlatform // {
- #    platform = hostPlatform.platform // {
- #      kernelBaseConfig = "nintendo-switch_defconfig";
- #      kernelAutoModules = false; # compilation failures otherwise
- #    };
- #  };
- 
- } // (args.argsOverride or {}))
+buildLinux {
+  inherit stdenv kernelPatches;
+  inherit hostPlatform;
+  inherit src;
+  inherit version;
+
+  modDirVersion = "4.16.0-rc1";
+
+  inherit configfile;
+
+  allowImportFromDerivation = true;
+}
+
+#{ stdenv, buildPackages, hostPlatform, fetchFromGitHub, perl, buildLinux, ... } @ args:
+#let
+#  src = fetchFromGitHub {
+#    owner = "fail0verflow";
+#    repo = "switch-linux";
+#    rev = "01b404c2112028a48ccf533b84218689032054c3";
+#    sha256 = "1x7bvc69rhnkx3cy4djsjk0hrc6ypdsnm3465v1gxmddd3fx1mjb";
+#  };
+#  #src = fetchFromGitHub {
+#  #  owner = "torvalds";
+#  #  repo = "linux";
+#  #  rev = "v4.16-rc1";
+#  #  sha256 = "0pckpg0xgmcn4a3ig1z8p3vrw9kzf9psa1f4z0aq6n5w91sw1m87";
+#  #};
+#  version = "4.16.0-rc1";
+#in
+#
+# buildLinux (args // {
+#   inherit version;
+#   inherit src;
+# 
+#   kernelPatches = args.kernelPatches;
+# 
+#   features.iwlwifi = true;
+#   features.efiBootStub = false;
+#   features.needsCifsUtils = true;
+#   features.netfilterRPFilter = true;
+#     #enableParallelBuilding = false;
+#
+# 
+# #  # https://github.com/NixOS/nixpkgs/issues/35166#issuecomment-366594016
+# #  # override aarch64-multiplatform settings.
+# #  # not the right way to do this; need advice.
+# #  hostPlatform = hostPlatform // {
+# #    platform = hostPlatform.platform // {
+# #      kernelBaseConfig = "nintendo-switch_defconfig";
+# #      kernelAutoModules = false; # compilation failures otherwise
+# #    };
+# #  };
+# 
+# } // (args.argsOverride or {}))
