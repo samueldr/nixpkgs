a75f6ef2efb464fd2354670ecea2bb567b00a085
Samuel Dionne-Riel <samuel@dionne-riel.com>
2021-04-03 18:43:06 -0400

    maliit-keyboard: Init at 2.0.0

--- pkgs/top-level/all-packages.nix
+++ pkgs/top-level/all-packages.nix
@@ -2888,6 +2888,8 @@ in
 
   maliit-framework = libsForQt5.callPackage ../applications/misc/maliit-framework { };
 
+  maliit-keyboard = libsForQt5.callPackage ../applications/misc/maliit-keyboard { };
+
   mapproxy = callPackage ../applications/misc/mapproxy { };
 
   marl = callPackage ../development/libraries/marl {};
