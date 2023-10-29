4c58e6d331b038b8fd1ce120f9f47ad14c83db6e
Raito Bezarius <masterancpp@gmail.com>
2023-10-29 12:58:14 +0100

    lib/systems: add UEFI triples
    With the new gnu-config support, we add UEFI system triples and system doubles in nixpkgs.

--- pkgs/development/libraries/gnu-config/default.nix
+++ pkgs/development/libraries/gnu-config/default.nix
@@ -23,6 +23,10 @@ in stdenv.mkDerivation {
   pname = "gnu-config";
   version = "2024-01-01";
 
+  patches = [
+    ./support-uefi-triples.patch
+  ];
+
   unpackPhase = ''
     runHook preUnpack
     cp ${configGuess} ./config.guess
