f4575d3ceaa9e2914aab0d52b09cc2d4d49be480
Samuel Dionne-Riel <samuel@dionne-riel.com>
2024-02-19 17:13:46 -0500

    edk2: Inherit targetArch in edk2.mkDerivation
    This allows usages to refer to e.g. the proper build path when adding to
    the build phase.

--- pkgs/development/compilers/edk2/default.nix
+++ pkgs/development/compilers/edk2/default.nix
@@ -103,6 +103,7 @@ edk2 = stdenv.mkDerivation rec {
   };
 
   passthru = {
+    inherit targetArch;
     mkDerivation = projectDscPath: attrsOrFun: stdenv.mkDerivation (finalAttrs:
     let
       attrs = lib.toFunction attrsOrFun finalAttrs;
